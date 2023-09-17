04e4ad2214e3a0b5b963e746a3dcf96f26f0ec77
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2023-09-17 19:46:12 +0200

    feat: button improve, photo desc

--- travel-map/src/components/organisms/gallery/ImageGallery.tsx
+++ travel-map/src/components/organisms/gallery/ImageGallery.tsx
@@ -1,4 +1,4 @@
-import ImageGallery from "react-image-gallery";
+import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
 import { getCityPhotos } from "../../../utils/photos";
 import { City } from "../../../classes/City";
 import { TextBackButton } from "../../atoms";
@@ -19,38 +19,67 @@ export function CustomImageGallery({
 }: CustomImageGalleryProps) {
   const items = getCityPhotos(currentCity?.name || "");
 
-  const renderItems = (item: any) => {
-    if (item.video) {
-      return (
-        <video
-          className="image-gallery-video"
-          controls
-          id={item.original}
-          key={item.original}
-        >
-          <source src={baseUrl + item.original} type="video/mp4" />
-        </video>
-      );
-    } else if (item.youtube) {
-      return (
-        <iframe
-          className="image-gallery-video"
-          src="https://www.youtube.com/embed/_CaBMaSUx_w"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
-      );
-    } else {
-      return (
-        <img
-          className="image-gallery-image"
-          src={baseUrl + item.original}
-          alt=""
-        />
-      );
-    }
+  type ItemType = ReactImageGalleryItem & {
+    video?: boolean;
+    youtube?: boolean;
+  };
+
+  const getDescription = (item: ItemType) => {
+    return (
+      <>
+        {item.description && (
+          <span
+            className="image-gallery-description"
+            style={{ left: "0px", right: "initial" }}
+          >
+            {item.description}
+          </span>
+        )}
+      </>
+    );
+  };
+
+  const renderItems = (item: ItemType) => {
+    const media = (item: ItemType) => {
+      if (item.video) {
+        return (
+          <video
+            className="image-gallery-video"
+            controls
+            id={item.original}
+            key={item.original}
+          >
+            <source src={baseUrl + item.original} type="video/mp4" />
+          </video>
+        );
+      } else if (item.youtube) {
+        return (
+          <iframe
+            className="image-gallery-video"
+            src="https://www.youtube.com/embed/_CaBMaSUx_w"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          />
+        );
+      } else {
+        return (
+          <img
+            className="image-gallery-image"
+            src={baseUrl + item.original}
+            alt=""
+          />
+        );
+      }
+    };
+
+    return (
+      <>
+        {media(item)}
+        {getDescription(item)}
+      </>
+    );
   };
 
   return (
