7a7d884584e902ec7bf124e2ad187dc24fd16b6b
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2023-10-29 23:02:11 +0100

    feat: new logo, added navbar and improved readability

--- travel-map/src/components/organisms/index.ts
+++ travel-map/src/components/organisms/index.ts
@@ -1,7 +1,8 @@
 import { Box } from "./box/Box";
 import { Tooltip } from "./tooltip/Tooltip";
 import { Footer } from "./footer/Footer";
+import { Navbar } from "./navbar/Navbar";
 import MapChart from "./map/Map";
 import { CustomImageGallery } from "./gallery/ImageGallery";
 
-export { Box, Tooltip, Footer, MapChart, CustomImageGallery };
+export { Box, Tooltip, Footer, MapChart, CustomImageGallery, Navbar };
