e052e3a1f6918167d886c39388f536cc7b46884c
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2023-12-03 16:08:46 +0100

    feat: import old right things

--- travel-map/src/main.tsx
+++ travel-map/src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import "./styles/global.scss";
+import "./styles/typography.scss";
+import "./styles/animations.scss";
+import "./styles/scrollbar.scss";
+import { Home } from "./components/pages";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
