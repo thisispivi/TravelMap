a029a0246f498f58dc54f7d95ea8da1d0aa77f9c
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-03-31 06:06:17 +0200

    feat: statically integration in json generator script

--- script/json-generator/lib/args.py
+++ script/json-generator/lib/args.py
@@ -10,26 +10,40 @@ def get_city_from_args(argumentList, logger):
         logger (Logger): The logger instance.
 
     Returns:
-        str: The city name.
+        dict: The dictionary with city name and statically path.
 
     Raises:
         Exception: If no arguments provided.
+        Exception: If only one argument provided.
     """
     argumentList = argumentList[1:]
-    options = "c:"
-    long_options = ["city"]
+    options = "c:s:"
+    long_options = ["city", "statically-path"]
     try:
         arguments, values = getopt.getopt(argumentList, options, long_options)
 
-        if (len(arguments) == 0):
+        if len(arguments) == 0:
             raise Exception(
-                "No arguments provided. Please provide city name with -c or --city flag")
+                "No arguments provided. Please provide city name with -c or --city flag and statically base path with -s or --statically-path flag"
+            )
 
+        if len(arguments) < 2:
+            raise Exception(
+                "Please provide both city name and statically base path with -c or --city and -s or --statically-path flags"
+            )
+
+        data = {}
         for currentArgument, currentValue in arguments:
             if currentArgument in ("-c", "--city"):
                 city = currentValue.capitalize()
                 logger.info("Generating json for city: %s", city)
-                return city
+                data["city"] = city
+            if currentArgument in ("-s", "--statically-path"):
+                path = currentValue
+                logger.info("Statically base path: %s", path)
+                data["statically_path"] = path
+
+        return data
     except getopt.error as err:
         logger.error(str(err))
         exit(2)
