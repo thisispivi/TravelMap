cd1333c582bedab4eb8c93e32bfec8ee9a3a6816
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-03-31 12:25:06 +0200

    feat: photos

--- travel-map/src/data/cities/Barcelona/photos/tr_110416_150416.ts
+++ travel-map/src/data/cities/Barcelona/photos/tr_110416_150416.ts
@@ -1,3 +1,508 @@
 import { Image } from "../../../../core";
 
-export const tr_110416_150416_images: Image[] = [];
+export const tr_110416_150416_images: Image[] = [
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/001.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/001t.jpg",
+    width: 1599,
+    height: 899,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/002.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/002t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/003.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/003t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/004.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/004t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/005.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/005t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/006.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/006t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/007.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/007t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/008.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/008t.jpg",
+    width: 683,
+    height: 281,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/009.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/009t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/010.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/010t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/011.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/011t.jpg",
+    width: 1366,
+    height: 451,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/012.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/012t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/013.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/013t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/014.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/014t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/015.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/015t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/016.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/016t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/017.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/017t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/018.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/018t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/019.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/019t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/020.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/020t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/021.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/021t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/022.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/022t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/023.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/023t.jpg",
+    width: 455,
+    height: 256,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/024.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/024t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/025.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/025t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/026.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/026t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/027.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/027t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/028.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/028t.jpg",
+    width: 1366,
+    height: 333,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/029.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/029t.jpg",
+    width: 1366,
+    height: 327,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/030.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/030t.jpg",
+    width: 1366,
+    height: 317,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/031.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/031t.jpg",
+    width: 1366,
+    height: 303,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/032.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/032t.jpg",
+    width: 683,
+    height: 191,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/033.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/033t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/034.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/034t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/035.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/035t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/036.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/036t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/037.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/037t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/038.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/038t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/039.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/039t.jpg",
+    width: 683,
+    height: 130,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/040.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/040t.jpg",
+    width: 686,
+    height: 915,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/041.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/041t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/042.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/042t.jpg",
+    width: 5,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/043.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/043t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/044.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/044t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/045.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/045t.jpg",
+    width: 733,
+    height: 977,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/046.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/046t.jpg",
+    width: 707,
+    height: 942,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/047.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/047t.jpg",
+    width: 726,
+    height: 967,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/048.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/048t.jpg",
+    width: 656,
+    height: 875,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/049.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/049t.jpg",
+    width: 362,
+    height: 483,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/050.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/050t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/051.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/051t.jpg",
+    width: 371,
+    height: 297,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/052.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/052t.jpg",
+    width: 1001,
+    height: 751,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/053.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/053t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/054.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/054t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/055.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/055t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/056.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Barcelona/056t.jpg",
+    width: 899,
+    height: 1123,
+  },
+];
--- travel-map/src/data/cities/Bruges/photos/tr_090823_090823.ts
+++ travel-map/src/data/cities/Bruges/photos/tr_090823_090823.ts
@@ -1,3 +1,166 @@
 import { Image } from "../../../../core";
 
-export const tr_090823_090823_images: Image[] = [];
+export const tr_090823_090823_images: Image[] = [
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/001.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/001t.jpg",
+    width: 1771,
+    height: 3148,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/002.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/002t.jpg",
+    width: 798,
+    height: 449,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/003.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/003t.jpg",
+    width: 3464,
+    height: 1949,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/004.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/004t.jpg",
+    width: 4,
+    height: 5,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/005.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/005t.jpg",
+    width: 3437,
+    height: 1933,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/006.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/006t.jpg",
+    width: 4,
+    height: 5,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/007.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/007t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/008.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/008t.jpg",
+    width: 1772,
+    height: 997,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/009.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/009t.jpg",
+    width: 4,
+    height: 5,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/010.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/010t.jpg",
+    width: 2616,
+    height: 2093,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/011.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/011t.jpg",
+    width: 3682,
+    height: 2071,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/012.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/012t.jpg",
+    width: 3454,
+    height: 1943,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/013.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/013t.jpg",
+    width: 733,
+    height: 1303,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/014.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/014t.jpg",
+    width: 2933,
+    height: 2200,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/015.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/015t.jpg",
+    width: 935,
+    height: 526,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/016.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/016t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/017.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/017t.jpg",
+    width: 64,
+    height: 85,
+  },
+  {
+    original:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/018.jpg",
+    alt: "",
+    thumbnail:
+      "https://cdn.statically.io/gh/iampivi/PhotoLake1/c3c78a72d14e8092556836839ec1dcf0dcdd4098/Bruges/018t.jpg",
+    width: 64,
+    height: 85,
+  },
+];
