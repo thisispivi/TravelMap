310295f1fe3c8b11e8621bdafa5efe82bb2855f2
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-04-07 19:03:03 +0200

    feat: Terni

---
+++ travel-map/src/data/cities/Terni/Terni.ts
@@ -0,0 +1,16 @@
+import { City, Travel } from "../../../core";
+import { Italy } from "../../countries/countries";
+import { tr_270122_270122_images } from "./photos/tr_270122_270122";
+
+export const Terni = new City({
+  name: "Terni",
+  country: Italy,
+  coordinates: [12.6411, 42.5635],
+  travels: [
+    new Travel({
+      sDate: new Date(2022, 1, 27),
+      eDate: new Date(2022, 1, 27),
+      photos: tr_270122_270122_images,
+    }),
+  ],
+});
