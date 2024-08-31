a848ccfe94b3d4d47a366c6a892619d53cf4a97e
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-08-31 18:25:36 +0200

    fix: small params refactor

--- travel-map/src/components/organisms/Lightbox/Lightbox.tsx
+++ travel-map/src/components/organisms/Lightbox/Lightbox.tsx
@@ -11,7 +11,7 @@ import {
   GalleryIcon,
 } from "../../../assets";
 import useLanguage from "../../../hooks/language/language";
-import { isShowPhotos } from "../../../utils/parameters";
+import { parameters } from "../../../utils/parameters";
 
 export interface LightboxProps {
   city: City;
@@ -44,7 +44,7 @@ export default function Lightbox(): JSX.Element {
       return (
         <iframe
           className="image-gallery-video"
-          src={isShowPhotos ? item.original : ""}
+          src={parameters.isShowPhotos ? item.original : ""}
           title={item.alt || ""}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
@@ -54,7 +54,7 @@ export default function Lightbox(): JSX.Element {
       return (
         <img
           className="image-gallery-image"
-          src={isShowPhotos ? item.original : ""}
+          src={parameters.isShowPhotos ? item.original : ""}
           alt=""
         />
       );
@@ -65,7 +65,7 @@ export default function Lightbox(): JSX.Element {
     (currentIndex: number | undefined) => {
       if (photoIdx !== undefined) {
         const element = document.querySelector(
-          `[aria-label="Go to Slide ${photoIdx + 1}"]`,
+          `[aria-label="Go to Slide ${photoIdx + 1}"]`
         );
         if (element) {
           const child = element.children[0];
@@ -80,13 +80,13 @@ export default function Lightbox(): JSX.Element {
         navigate(`../${currentIndex}`);
       }
     },
-    [photoIdx, navigate],
+    [photoIdx, navigate]
   );
 
   const renderNavigationButton = (
     onClick: MouseEventHandler<HTMLElement>,
     disabled: boolean,
-    direction: "left" | "right",
+    direction: "left" | "right"
   ) => (
     <Button
       className={`image-gallery-icon image-gallery-${direction}-nav ${
