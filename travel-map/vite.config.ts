d530d834a89e6a3b402cba8778c3c3360cf43ebf
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-08-31 21:44:37 +0200

    fix

--- travel-map/vite.config.ts
+++ travel-map/vite.config.ts
@@ -8,7 +8,7 @@ import { qrcode } from "vite-plugin-qrcode";
 export default defineConfig(() => {
   return defineConfig({
     plugins: [react(), svgr(), nodeResolve(), qrcode()],
-    base: "/",
+    base: "/TravelMap/",
     server: {
       watch: {
         usePolling: true,
