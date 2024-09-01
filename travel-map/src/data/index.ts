5a709302dfb0a168dd541ca1051f999ebb0b7c3a
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-09-01 20:42:37 +0200

    feat: added cities

--- travel-map/src/data/index.ts
+++ travel-map/src/data/index.ts
@@ -7,6 +7,7 @@ import { Bruges } from "./cities/Bruges/Bruges";
 import { Brussels } from "./cities/Brussels/Brussels";
 import { Budapest } from "./cities/Budapest/Budapest";
 import { Cefalù } from "./cities/Cefalù/Cefalù";
+import { Fujikawaguchiko } from "./cities/Fujikawaguchiko/Fujikawaguchiko";
 import { Genoa } from "./cities/Genoa/Genoa";
 import { Himeji } from "./cities/Himeji/Himeji";
 import { Imola } from "./cities/Imola/Imola";
@@ -17,6 +18,7 @@ import { London } from "./cities/London/London";
 import { Matsumoto } from "./cities/Matsumoto/Matsumoto";
 import { Nara } from "./cities/Nara/Nara";
 import { Osaka } from "./cities/Osaka/Osaka";
+import { Oshino } from "./cities/Oshino/Oshino";
 import { Porto } from "./cities/Porto/Porto";
 import { Rome } from "./cities/Rome/Rome";
 import { Shirakawago } from "./cities/Shirakawago/Shirakawago";
@@ -63,6 +65,8 @@ export const visitedCities: City[] = [
   Porto,
   Braga,
   Tokyo,
+  Oshino,
+  Fujikawaguchiko,
   Matsumoto,
   Takayama,
   Shirakawago,
