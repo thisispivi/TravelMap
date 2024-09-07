461ee8e6882689c1507a7c8f18404a1ea2e2dacf
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-09-07 19:00:54 +0200

    feat: fujikawaguchiko

---
+++ travel-map/src/data/cities/Fujikawaguchiko/photos/tr_150824_150824.ts
@@ -0,0 +1,283 @@
+import { Image } from "../../../../core";
+
+export const tr_150824_150824_images: Image[] = [
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/001.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/001t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/002.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/002t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/003.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/003t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/004.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/004t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/005.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/005t.jpg",
+    width: 64,
+    height: 85,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/006.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/006t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/007.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/007t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/008.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/008t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/009.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/009t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/010.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/010t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/011.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/011t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/012.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/012t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/013.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/013t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/014.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/014t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/015.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/015t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/016.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/016t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/017.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/017t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/018.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/018t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://youtube.com/embed/HaLnem5L2B4",
+    alt: "Fujikawaguchiko Strange guy (15/08/24)",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/019t.jpg",
+    width: 9,
+    height: 16,
+    youtube: true,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/020.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/020t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://youtube.com/embed/xRj_1VsglcE",
+    alt: "Fujikawaguchiko ropeway (15/08/24)",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/021t.jpg",
+    width: 9,
+    height: 16,
+    youtube: true,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/022.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/022t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/023.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/023t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/024.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/024t.jpg",
+    width: 433,
+    height: 576,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/025.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/025t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/026.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/026t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/027.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/027t.jpg",
+    width: 64,
+    height: 85,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/028.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/028t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/029.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/029t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/030.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/030t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/031.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Fujikawaguchiko/031t.jpg",
+    width: 3,
+    height: 4,
+  },
+];
