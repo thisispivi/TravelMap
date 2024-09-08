15e6085ae06a8b5c4361cd4e9170530ad763558c
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-09-08 12:01:18 +0200

    feat: shirakawago

--- travel-map/src/data/cities/Shirakawago/photos/tr_190824_190824.ts
+++ travel-map/src/data/cities/Shirakawago/photos/tr_190824_190824.ts
@@ -1,3 +1,300 @@
 import { Image } from "../../../../core";
 
-export const tr_190824_190824_images: Image[] = [];
+export const tr_190824_190824_images: Image[] = [
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/001.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/001t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/002.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/002t.jpg",
+    width: 2867,
+    height: 3822,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/003.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/003t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/004.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/004t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/005.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/005t.jpg",
+    width: 64,
+    height: 85,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/006.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/006t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/007.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/007t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/008.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/008t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/009.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/009t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/010.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/010t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/011.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/011t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/012.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/012t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/013.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/013t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/014.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/014t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/015.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/015t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/016.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/016t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/017.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/017t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/018.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/018t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/019.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/019t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/020.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/020t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/021.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/021t.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/022.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/022t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/023.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/023t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/024.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/024t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/025.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/025t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/026.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/026t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/027.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/027t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/028.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/028t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/029.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/029t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/030.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/030t.jpg",
+    width: 64,
+    height: 85,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/031.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/031t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/032.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/032t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/033.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/033t.jpg",
+    width: 64,
+    height: 85,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/034.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/034t.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/035.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/035t.jpg",
+    width: 64,
+    height: 85,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/036.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/036t.jpg",
+    width: 85,
+    height: 64,
+  },
+  {
+    original: "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/037.jpg",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Shirakawago/037t.jpg",
+    width: 85,
+    height: 64,
+  },
+];
