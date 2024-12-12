b2a181c831122d448caaeddaa2b3f0acda192471
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-12-12 22:03:58 +0100

    fix: map coordinates and package fix

--- travel-map/src/data/Malta/Sliema/Sliema.ts
+++ travel-map/src/data/Malta/Sliema/Sliema.ts
@@ -15,5 +15,5 @@ export const Sliema = new City({
   backgroundImgsSrc: [
     "https://pivi-travel-map.b-cdn.net/TravelMap/Backgrounds/Cities/Sliema.jpg",
   ],
-  mapCoordinates: [8, 45],
+  mapCoordinates: [10.9, 36.1],
 });
