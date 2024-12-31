280f2089e1a3fb91fe7483b316619bf81162be89
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-12-31 01:08:32 +0100

    fix: small css fix

--- travel-map/src/components/organisms/index.ts
+++ travel-map/src/components/organisms/index.ts
@@ -2,23 +2,17 @@ import { lazy } from "react";
 
 import Gallery from "./Gallery/Gallery";
 import InfoTab from "./InfoTab/InfoTab";
-const InfoTabCities = lazy(
-  () => import("./InfoTab/InfoTabCities/InfoTabCities")
-);
-const InfoTabFuture = lazy(
-  () => import("./InfoTab/InfoTabCities/InfoTabFuture")
-);
-const InfoTabLived = lazy(() => import("./InfoTab/InfoTabCities/InfoTabLived"));
-const InfoTabVisited = lazy(
-  () => import("./InfoTab/InfoTabCities/InfoTabVisited")
-);
-const InfoTabStats = lazy(() => import("./InfoTab/InfoTabStats/InfoTabStats"));
+import InfoTabCities from "./InfoTab/InfoTabCities/InfoTabCities";
+import InfoTabFuture from "./InfoTab/InfoTabCities/InfoTabFuture";
+import InfoTabLived from "./InfoTab/InfoTabCities/InfoTabLived";
+import InfoTabVisited from "./InfoTab/InfoTabCities/InfoTabVisited";
+import InfoTabStats from "./InfoTab/InfoTabStats/InfoTabStats";
 import LanguageSelector from "./Language/Language";
 import LeftBar from "./LeftBar/LeftBar";
 import Lightbox from "./Lightbox/Lightbox";
+import MapTooltip from "./Tooltip/TooltipMap";
+import Tooltip from "./Tooltip/Tooltip";
 const Map = lazy(() => import("./Map/Map"));
-const MapTooltip = lazy(() => import("./Tooltip/TooltipMap"));
-const Tooltip = lazy(() => import("./Tooltip/Tooltip"));
 
 export {
   Gallery,
