77843e132b06c1e2b988f9fde7476b52e85c8862
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2025-01-06 15:23:49 +0100

    fix

--- travel-map/src/data/Malta/Malta.ts
+++ travel-map/src/data/Malta/Malta.ts
@@ -3,5 +3,5 @@ export const Malta = new Country(
   "Malta",
   { h: 352, s: 77, l: 46 },
   Continent.Europe,
-  0.01
+  0.01,
 );
--- travel-map/src/data/Portugal/Portugal.ts
+++ travel-map/src/data/Portugal/Portugal.ts
@@ -2,5 +2,5 @@ import { Continent, Country } from "../../core";
 export const Portugal = new Country(
   "Portugal",
   { h: 120, s: 86, l: 23 },
-  Continent.Europe
+  Continent.Europe,
 );
