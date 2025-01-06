239ee26cdd3897b1a92a025f7dec5e78d5c263d9
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2025-01-06 15:23:49 +0100

    fix

--- travel-map/src/data/Italy/Italy.ts
+++ travel-map/src/data/Italy/Italy.ts
@@ -2,5 +2,5 @@ import { Continent, Country } from "../../core";
 export const Italy = new Country(
   "Italy",
   { h: 228, s: 64, l: 45 },
-  Continent.Europe
+  Continent.Europe,
 );
--- travel-map/src/data/Spain/Spain.ts
+++ travel-map/src/data/Spain/Spain.ts
@@ -2,5 +2,5 @@ import { Continent, Country } from "../../core";
 export const Spain = new Country(
   "Spain",
   { h: 46, s: 100, l: 52 },
-  Continent.Europe
+  Continent.Europe,
 );
