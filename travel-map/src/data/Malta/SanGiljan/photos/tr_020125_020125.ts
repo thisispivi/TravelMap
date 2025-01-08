4fb520f2cd11713b26eb6fa5d1f841c929b64f58
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2025-01-08 01:12:47 +0100

    feat: Mdina, Rabat, Valletta and SanGiljan

--- travel-map/src/data/Malta/SanGiljan/photos/tr_020125_020125.ts
+++ travel-map/src/data/Malta/SanGiljan/photos/tr_020125_020125.ts
@@ -1,3 +1,103 @@
 import { Image } from "../../../../core";
 
-export const tr_020125_020125_images: Image[] = [];
+export const tr_020125_020125_images: Image[] = [
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/001c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/001t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/002c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/002t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/003c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/003t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/004c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/004t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/005c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/005t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/006c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/006t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/007c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/007t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/008c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/008t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/009c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/009t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/010c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/010t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/011c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/SanGiljan/011t.webp",
+    width: 753,
+    height: 1000,
+  },
+];
--- travel-map/src/data/Malta/Valletta/photos/tr_020125_020125.ts
+++ travel-map/src/data/Malta/Valletta/photos/tr_020125_020125.ts
@@ -1,3 +1,535 @@
 import { Image } from "../../../../core";
 
-export const tr_020125_020125_images: Image[] = [];
+export const tr_020125_020125_images: Image[] = [
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/001c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/001t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/002c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/002t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/003c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/003t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/004c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/004t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/005c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/005t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/006c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/006t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/007c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/007t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/008c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/008t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/009c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/009t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/010c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/010t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/011c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/011t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/012c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/012t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/013c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/013t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/014c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/014t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/015c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/015t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/016c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/016t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/017c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/017t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/018c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/018t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/019c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/019t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/020c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/020t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/021c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/021t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/022c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/022t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/023c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/023t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/024c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/024t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/025c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/025t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/026c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/026t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/027c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/027t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/028c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/028t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/029c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/029t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/030c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/030t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/031c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/031t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/032c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/032t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/033c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/033t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/034c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/034t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/035c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/035t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/036c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/036t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/037c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/037t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/038c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/038t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/039c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/039t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/040c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/040t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/041c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/041t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://youtube.com/embed/kTjv789qD_A",
+    alt: "Valletta - Triton Fountain (02/05/2025)",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/042t.webp",
+    width: 9,
+    height: 16,
+    youtube: true,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/043c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/043t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/044c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/044t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/045c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/045t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/046c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/046t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/047c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/047t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original: "https://youtube.com/embed/q-0OFcSx8HY",
+    alt: "Valletta - On top of ferris wheel (02/05/2025)",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/048t.webp",
+    width: 9,
+    height: 16,
+    youtube: true,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/049c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/049t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/050c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/050t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/051c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/051t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/052c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/052t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/053c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/053t.webp",
+    width: 4,
+    height: 3,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/054c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/054t.webp",
+    width: 9,
+    height: 16,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/055c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/055t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/056c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/056t.webp",
+    width: 9,
+    height: 16,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/057c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/057t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/058c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/058t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/059c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Valletta/059t.webp",
+    width: 3,
+    height: 4,
+  },
+];
