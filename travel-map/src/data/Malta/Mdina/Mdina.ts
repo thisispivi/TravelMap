a6913d8b4cf7cbfd22155b7ccaafaa68b4af5ba1
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2025-01-08 01:12:47 +0100

    feat: Mdina, Rabat, Valletta and SanGiljan

--- travel-map/src/data/Malta/Mdina/Mdina.ts
+++ travel-map/src/data/Malta/Mdina/Mdina.ts
@@ -1,5 +1,6 @@
 import { City, Travel } from "../../../core";
 import { Malta } from "../Malta";
+import { tr_020125_020125_images } from "./photos/tr_020125_020125";
 
 export const Mdina = new City({
   name: "Mdina",
@@ -9,6 +10,7 @@ export const Mdina = new City({
     new Travel({
       sDate: new Date(2025, 0, 2),
       eDate: new Date(2025, 0, 2),
+      photos: tr_020125_020125_images,
     }),
   ],
   backgroundImgsSrc: [
