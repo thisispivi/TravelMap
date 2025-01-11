6624fc1dbdfa186ff69c144547756f56c0cacf0f
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2025-01-11 13:29:11 +0100

    feat: Malta done

--- travel-map/src/data/Malta/Comino/photos/tr_040125_040125.ts
+++ travel-map/src/data/Malta/Comino/photos/tr_040125_040125.ts
@@ -1,3 +1,526 @@
 import { Image } from "../../../../core";
 
-export const tr_040125_040125_images: Image[] = [];
+export const tr_040125_040125_images: Image[] = [
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/001c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/001t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/002c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/002t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/003c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/003t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/004c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/004t.webp",
+    width: 5,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/005c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/005t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/006c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/006t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/007c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/007t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/008c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/008t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/009c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/009t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/010c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/010t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/011c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/011t.webp",
+    width: 2000,
+    height: 749,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/012c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/012t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/013c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/013t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/014c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/014t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/015c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/015t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/016c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/016t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/017c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/017t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/018c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/018t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/019c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/019t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/020c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/020t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/021c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/021t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/022c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/022t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/023c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/023t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/024c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/024t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/025c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/025t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/026c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/026t.webp",
+    width: 500,
+    height: 391,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/027c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/027t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/028c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/028t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/029c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/029t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/030c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/030t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/031c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/031t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/032c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/032t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/033c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/033t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/034c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/034t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/035c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/035t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/036c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/036t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/037c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/037t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/038c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/038t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/039c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/039t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/040c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/040t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/041c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/041t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/042c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/042t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/043c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/043t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/044c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/044t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/045c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/045t.webp",
+    width: 753,
+    height: 1000,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/046c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/046t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/047c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/047t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/048c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/048t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/049c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/049t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/050c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/050t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/051c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/051t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/052c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/052t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/053c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/053t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/054c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/054t.webp",
+    width: 1000,
+    height: 753,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/055c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/055t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/056c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/056t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/057c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/057t.webp",
+    width: 3,
+    height: 4,
+  },
+  {
+    original:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/058c.webp",
+    alt: "",
+    thumbnail:
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Malta/Comino/058t.webp",
+    width: 3,
+    height: 4,
+  },
+];
