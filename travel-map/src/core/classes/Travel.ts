da24cc6055949fffbc03f87c6c388ce1d38afc1b
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2025-01-11 13:53:17 +0100

    fix: parameters zoom especially for mobile, improved css and gallery layout

--- travel-map/src/core/classes/Travel.ts
+++ travel-map/src/core/classes/Travel.ts
@@ -5,6 +5,8 @@ export interface TravelInterface {
   eDate: Date;
   photos?: Image[];
   isFuture?: boolean;
+  rowCostraints?: { minPhotos?: number; maxPhotos?: number };
+  targetRowHeight?: number;
 }
 
 /**
@@ -19,17 +21,32 @@ export interface TravelInterface {
  * @param {Date} travelData.eDate - The end date of the travel
  * @param {Image[]} travelData.photos - The photos of the travel
  * @param {boolean} travelData.isFuture - If the travel is in the future
+ * @param {object} travelData.rowCostraints - The constraints of the rows of the gallery
+ * @param {number} travelData.rowCostraints.minPhotos - The minimum number of photos per row
+ * @param {number} travelData.rowCostraints.maxPhotos - The maximum number of photos per row
+ * @param {number} travelData.targetRowHeight - The target height of the rows of the gallery
  */
 export class Travel implements TravelInterface {
   sDate: Date;
   eDate: Date;
   photos: Image[];
   isFuture: boolean = false;
+  rowCostraints?: { minPhotos?: number; maxPhotos?: number };
+  targetRowHeight?: number;
 
-  constructor({ sDate, eDate, photos, isFuture }: TravelInterface) {
+  constructor({
+    sDate,
+    eDate,
+    photos,
+    isFuture,
+    rowCostraints,
+    targetRowHeight,
+  }: TravelInterface) {
     this.sDate = sDate;
     this.eDate = eDate;
     this.photos = photos || [];
     this.isFuture = isFuture || false;
+    this.rowCostraints = rowCostraints || { maxPhotos: 8 };
+    this.targetRowHeight = targetRowHeight || 260;
   }
 }
