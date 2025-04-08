ba7157f04c79f4937cd7066f9b8f65cf6afeba7b
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2025-04-08 20:19:27 +0200

    fix: rome images

--- travel-map/src/data/Italy/Rome/photos/tr_100824_120824.ts
+++ travel-map/src/data/Italy/Rome/photos/tr_100824_120824.ts
@@ -3,253 +3,253 @@ import { Image } from "../../../../core";
 export const tr_100824_120824_images: Image[] = [
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/001c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/001c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/001t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/001t.webp",
     width: 433,
     height: 576,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/002c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/002c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/002t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/002t.webp",
     width: 64,
     height: 85,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/003c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/003c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/003t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/003t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/004c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/004c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/004t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/004t.webp",
     width: 85,
     height: 64,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/005c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/005c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/005t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/005t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/006c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/006c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/006t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/006t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/007c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/007c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/007t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/007t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/008c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/008c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/008t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/008t.webp",
     width: 64,
     height: 85,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/009c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/009c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/009t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/009t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/010c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/010c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/010t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/010t.webp",
     width: 85,
     height: 64,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/011c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/011c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/011t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/011t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/012c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/012c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/012t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/012t.webp",
     width: 85,
     height: 64,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/013c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/013c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/013t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/013t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/014c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/014c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/014t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/014t.webp",
     width: 576,
     height: 433,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/015c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/015c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/015t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/015t.webp",
     width: 64,
     height: 85,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/016c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/016c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/016t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/016t.webp",
     width: 576,
     height: 433,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/017c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/017c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/017t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/017t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/018c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/018c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/018t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/018t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/019c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/019c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/019t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/019t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/020c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/020c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/020t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/020t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/021c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/021c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/021t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/021t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/022c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/022c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/022t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/022t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/023c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/023c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/023t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/023t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/024c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/024c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/024t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/024t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/025c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/025c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/025t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/025t.webp",
     width: 64,
     height: 85,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/026c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/026c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/026t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/026t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/027c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/027c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/027t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/027t.webp",
     width: 3,
     height: 4,
   },
   {
     original:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/028c.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/028c.webp",
     alt: "",
     thumbnail:
-      "https:/pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/028t.webp",
+      "https://pivi-travel-map.b-cdn.net/TravelMap/Travels/Italy/Rome1/028t.webp",
     width: 64,
     height: 85,
   },
