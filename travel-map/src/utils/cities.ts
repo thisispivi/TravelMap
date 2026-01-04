8752ca65afc055aae361e4153f377c4cb7352996
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2026-01-04 02:05:03 +0100

    fix: add missing commas for consistency and to prevent potential syntax errors

--- travel-map/src/utils/cities.ts
+++ travel-map/src/utils/cities.ts
@@ -28,15 +28,15 @@ export function getCitiesByCountriesAndIsFuture({
         filter(
           (city) =>
             city.country.id === country.id &&
-            city.travels.some((travel) => travel.isFuture === isFuture)
+            city.travels.some((travel) => travel.isFuture === isFuture),
         ),
         flatMap((city) =>
           city.travels
             .filter((travel) => travel.isFuture === isFuture)
-            .map((travel) => new City({ ...city, travels: [travel] }))
-        )
-      )
-    )
+            .map((travel) => new City({ ...city, travels: [travel] })),
+        ),
+      ),
+    ),
   );
 }
 
@@ -61,7 +61,7 @@ export function getTotalMediaTaken(cities: City[]): number {
   return pipe(
     cities,
     flatMap((city) => city.travels),
-    sumBy((travel) => travel.photos.length)
+    sumBy((travel) => travel.photos.length),
   );
 }
 
@@ -75,7 +75,7 @@ type GroupCitiesByYearOptions = { cutoffYear: number };
  */
 export function groupCitiesByYear(
   cities: City[],
-  { cutoffYear }: GroupCitiesByYearOptions
+  { cutoffYear }: GroupCitiesByYearOptions,
 ): Record<number, City[]> {
   const result: Record<number, City[]> = {};
 
