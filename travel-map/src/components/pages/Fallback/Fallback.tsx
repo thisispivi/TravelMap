9b96e04548a510ceb735eca16c0515d5046ab5f3
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2026-01-17 12:05:29 +0100

    feat: implement chunk recovery mechanism in Fallback component and add storage utility functions

--- travel-map/src/components/pages/Fallback/Fallback.tsx
+++ travel-map/src/components/pages/Fallback/Fallback.tsx
@@ -2,10 +2,11 @@ import { useNavigate, useRouteError } from "react-router-dom";
 import { Button } from "../../atoms";
 import { useTranslation } from "react-i18next";
 import "./Fallback.scss";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { mobileAndTabletCheck } from "@/utils/responsive";
 import useResponsive from "@/hooks/style/responsive";
 import useThemeDetector from "@/hooks/style/theme";
+import { getWithExpiry, setWithExpiry } from "@/utils/storage";
 
 /**
  * Fallback Component
@@ -23,13 +24,24 @@ export default function Fallback(): ReactNode | null {
   const navigate = useNavigate();
   const { isDarkTheme } = useThemeDetector();
 
-  if (!routerError) return null;
   const error =
     typeof routerError === "object" &&
     routerError !== null &&
     "error" in routerError
       ? (routerError as { error: Error }).error
       : (routerError as Error);
+
+  useEffect(() => {
+    const DYNAMIC_IMPORT_FAILURE_RE =
+      /Failed to fetch dynamically imported module|Importing a module script failed|ChunkLoadError|Loading chunk \d+ failed/i;
+    if (!error || !(error instanceof Error)) return;
+    if (error?.message && DYNAMIC_IMPORT_FAILURE_RE.test(error.message)) {
+      if (!getWithExpiry("chunk_failed")) {
+        setWithExpiry("chunk_failed", "true", 10000);
+        window.location.reload();
+      }
+    }
+  }, [error]);
 
   const isShowStack = !(
     mobileAndTabletCheck() || responsive.window.width <= 980
