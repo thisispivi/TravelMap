c19e3377a83a097574db8b1bab40314c121a691d
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2026-01-18 18:29:20 +0100

    refactor: Enhance TripCard component with animations and accessibility improvements
    - Updated TripCard.scss to improve styling and transitions for better user experience.
    - Refactored TripCard.tsx to utilize framer-motion for animations and added accessibility features such as aria-controls and aria-expanded.
    - Simplified the logic for handling country flags and city details in TripCard.
    - Removed unnecessary state management for countries in InfoTabTrips and cleaned up related code.
    - Updated InfoTabVisited to remove unused props and commented-out code.
    - Corrected the name of the "Cote d'Azur Trip" to "Côte d'Azur Trip" in data.

--- travel-map/src/components/organisms/InfoTab/InfoTabTrips/InfoTabVisited.tsx
+++ travel-map/src/components/organisms/InfoTab/InfoTabTrips/InfoTabVisited.tsx
@@ -1,6 +1,6 @@
 import { JSX } from "react";
 import "./InfoTabVisited.scss";
-import { visitedCountries, visitedTrips } from "@/data";
+import { visitedTrips } from "@/data";
 import InfoTabTrips from "./InfoTabTrips";
 
 interface InfoTabVisitedProps {
@@ -25,33 +25,8 @@ export default function InfoTabVisited({
   className = "",
   isVisible = false,
 }: InfoTabVisitedProps): JSX.Element {
-  // const allCities = useMemo(() => {
-  //   const result = getCitiesByCountriesAndIsFuture({
-  //     cities: visitedCities,
-  //     countries: visitedCountries,
-  //     isFuture: false,
-  //   });
-
-  //   return [...result].sort(sortByTravelStartDate);
-  // }, []);
-
-  // const travelsByCityName = useMemo(() => {
-  //   return new globalThis.Map<string, Travel[]>(
-  //     visitedCities.map((city) => [city.name, city.travels]),
-  //   );
-  // }, []);
-
-  // const getTravelIdx = useCallback(
-  //   (city: City, travel: Travel) => {
-  //     const travels = travelsByCityName.get(city.name);
-  //     return travels ? travels.indexOf(travel) : -1;
-  //   },
-  //   [travelsByCityName],
-  // );
-
   return (
     <InfoTabTrips
-      allCountries={visitedCountries}
       className={className}
       id="visited"
       isVisible={isVisible}
