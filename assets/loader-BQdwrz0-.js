18c129a914cc8dd23928f04ab6341c0f283f4fab
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2026-01-28 22:24:35 +0100

    Updates

---
+++ assets/loader-BQdwrz0-.js
@@ -0,0 +1,1 @@
+import{v as t}from"./data-DhbgoilD.js";import"./vendor-BNBKChTz.js";function r(r){const{cityName:a,travelIdx:n}=r.params,e=t.find(t=>t.name===a);return e&&n?{city:e,travelIdx:parseInt(n)}:null}export{r as loader};
