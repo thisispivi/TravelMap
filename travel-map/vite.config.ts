980d601217ad88fd79d261aecdfc4ed4947b450e
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2026-01-28 22:23:05 +0100

    chore: remove PurgeCSS plugin from Vite configuration to streamline build process

--- travel-map/vite.config.ts
+++ travel-map/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
-import type { Plugin } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import autoprefixer from "autoprefixer";
 import { qrcode } from "vite-plugin-qrcode";
 import { resolve } from "path";
-import PurgeCSS from "vite-plugin-purgecss";
 
 export default defineConfig({
   plugins: [
@@ -16,14 +14,6 @@ export default defineConfig({
     svgr(),
     nodeResolve(),
     qrcode(),
-    PurgeCSS({
-      content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      safelist: {
-        standard: [/^apexcharts/, /^carousel/, /^image-gallery/],
-        deep: [/^apexcharts/, /^carousel/, /^image-gallery/],
-      },
-    }) as unknown as Plugin,
   ],
   base: "/",
   server: { watch: { usePolling: true }, host: true },
