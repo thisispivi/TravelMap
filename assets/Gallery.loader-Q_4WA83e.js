4a63545c87a31c39dbe93cf30c40624f57a14c9e
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2026-09-14 01:33:11 +0200

    Updates

--- assets/Gallery.loader-Q_4WA83e.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-Cs1oNtBx.js";function t(t){let{cityName:n,travelIdx:r}=t.params,i=e.find(e=>e.name===n);return!i||!r?null:{city:i,travelIdx:parseInt(r,10)}}export{t as galleryLoader};
---
+++ assets/Row-CQAa8CaT.js
@@ -0,0 +1,1 @@
+import{s as e}from"./framer-CU7KdSdW.js";import{T as t}from"./index-Ck7ffA-c.js";var n=e();function r({className:e=``,children:r}){return(0,n.jsx)(`div`,{className:t(`row`,e),children:r})}export{r as t};
